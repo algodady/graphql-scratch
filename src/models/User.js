af79ff713a8c033be658e87796b14d175e2e0b54
akash kumar <akash@arivani.net>
2025-04-10 15:21:30 +0530

    test

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// Define User Schema for MongoDB
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
